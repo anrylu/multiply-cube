7e57b4985cd97d5e43c4fd8537ff1da4dd8e32af
anrylu <mengting.lu@gmail.com>
2020-01-24 15:38:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# multiply-cube
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/multiply-cube)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, MatTabsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
