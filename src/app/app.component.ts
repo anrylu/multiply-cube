9a4d3b87365508efcbfd3b434d64019c7ee84252
anrylu <mengting.lu@gmail.com>
2020-01-25 12:16:20 +0800

    add basic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  configForm;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.configForm = this.formBuilder.group({
+      multiplication_count: 9,
+      face_count: 6
+    });
+  }
+ 
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+    this.configForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MultiplicationTableComponent } from './multiplication-table/multiplication-table.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatTabsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MatTabsModule, MatTableModule ],
+  declarations: [ AppComponent, HelloComponent, MultiplicationTableComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
