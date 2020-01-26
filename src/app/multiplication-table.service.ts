5f9b2804419344d1d09f0478d94896fb44509739
anrylu <mengting.lu@gmail.com>
2020-01-26 19:46:16 +0800

    add unique number color

--- src/app/multiplication-table.service.ts
+++ src/app/multiplication-table.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
+export interface ResultElement {
+  value: number;
+  is_unique: boolean;
+}
 export interface MultiplyElement {
   multiplier: number;
-  result: number[];
-  is_unique: boolean[];
+  result: ResultElement[];
 }
 
 @Injectable()
@@ -15,19 +18,36 @@ export class MultiplicationTableService {
   // result
   multiplers: number[] = [];
   data: MultiplyElement[]  = [];
+  unique_values: number[] = [];
 
   calculate() {
     var element: MultiplyElement;
+    var result_element: ResultElement;
 
+    // reset data
     this.multiplers = [];
     this.data = [];
+    this.unique_values = [];
+
+    // generate data
     for( var i=1; i<=this.multiplication_count; i++ ) {
       this.multiplers.push(i);
-      element = {multiplier: i, result: [], is_unique: []};
+      element = {multiplier: i, result: []};
       for( var j=1; j<=this.multiplication_count; j++ ) {
-        element.result.push(i*j);
+        result_element = {value: i*j, is_unique: false};
+        element.result.push(result_element);
       }
       this.data.push(element);
+    }
+
+    // check unique
+    for( var i=0; i<this.multiplication_count; i++ ) {
+      for( var j=0; j<this.multiplication_count; j++ ) {
+        if( this.unique_values.indexOf(this.data[j].result[i].value) < 0 ) {
+          this.unique_values.push(this.data[j].result[i].value);
+          this.data[j].result[i].is_unique = true;
+        }
+      }
     }
   }
 
