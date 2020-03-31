11a8cd7d3ff2e18259919f3ed4263bae2ea11eaa
anrylu <mengting.lu@gmail.com>
2020-03-31 23:30:42 +0800

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HelloComponent } from './hello.component';
 import { MultiplicationTableComponent } from './multiplication-table/multiplication-table.component';
 import { MultiplicationTableService } from './multiplication-table.service';
 import { NumberAllocationComponent } from './number-allocation/number-allocation.component';
+import { NumberAllocationAggregateComponent } from './number-allocation-aggregate/number-allocation-aggregate.component';
 
 @NgModule({
   imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MatTabsModule ],
-  declarations: [ AppComponent, HelloComponent, MultiplicationTableComponent, NumberAllocationComponent ],
+  declarations: [ AppComponent, HelloComponent, MultiplicationTableComponent, NumberAllocationComponent, NumberAllocationAggregateComponent ],
   bootstrap:    [ AppComponent ],
   providers: [MultiplicationTableService]
 })
---
+++ src/app/number-allocation-aggregate/number-allocation-aggregate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number-allocation-aggregate',
+  templateUrl: './number-allocation-aggregate.component.html',
+  styleUrls: ['./number-allocation-aggregate.component.css']
+})
+export class NumberAllocationAggregateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
